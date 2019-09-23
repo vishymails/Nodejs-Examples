a5ecf97483ece76aa5b2b439c986359c0a4e250a
vishymails <vishymails@gmail.com>
2019-09-23 11:40:38 +0530

    Add files via upload

---
+++ queryStringExample.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url);
+    res.end();
+}).listen(8080); 
+
+//to execute type http://localhost:8080/vishwanath
