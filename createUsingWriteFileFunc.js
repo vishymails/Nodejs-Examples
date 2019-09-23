89ef647e3ced79423606c570ac60149023bd4dc7
vishymails <vishymails@gmail.com>
2019-09-23 13:44:29 +0530

    Add files via upload

---
+++ createUsingWriteFileFunc.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+}); 
+//The fs.writeFile() method replaces the specified file and content if it exists. If the file does not exist, a new file, containing the specified content, will be created
+
